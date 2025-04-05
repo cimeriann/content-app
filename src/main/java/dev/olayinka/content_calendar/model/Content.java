11b3668fffd0760de1ae97f1dffa724c25d543ce
Olayinka <adebisiolayinka30@gmail.com>
2025-04-06 00:04:40 +0100

    create content class

---
+++ src/main/java/dev/olayinka/content_calendar/model/Content.java
@@ -0,0 +1,16 @@
+package dev.olayinka.content_calendar.model;
+
+import java.time.LocalDateTime;
+
+public record Content(
+        Integer id,
+        String title,
+        String desc,
+        Status status,
+        Type contentType,
+        LocalDateTime dateCreated,
+        LocalDateTime dateUpdated,
+        String url
+) {
+
+}
