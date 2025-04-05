68c85866e9c5ff25f16670888e073751c7981416
Olayinka <adebisiolayinka30@gmail.com>
2025-04-06 00:05:29 +0100

    create content controller

---
+++ src/main/java/dev/olayinka/content_calendar/controller/ContentController.java
@@ -0,0 +1,53 @@
+package dev.olayinka.content_calendar.controller;
+
+import dev.olayinka.content_calendar.model.Content;
+import dev.olayinka.content_calendar.repository.ContentCollectionRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/content")
+@CrossOrigin
+public class ContentController {
+    private final ContentCollectionRepository repository;
+
+    public ContentController(ContentCollectionRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("")
+    public List<Content> findAll(){
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Content findById(@PathVariable Integer id){
+        return repository.findById(id).
+                orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Content Not Found"));
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    public void create(@RequestBody Content content) {
+        repository.save(content);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    public void update(@RequestBody Content content, @PathVariable Integer id){
+        if (!repository.existsById(id)){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Content Not Found");
+        }
+        repository.save(content);
+    }
+
+    @ResponseStatus(HttpStatus.GONE)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer id){
+        repository.deleteById(id);
+    }
+}
